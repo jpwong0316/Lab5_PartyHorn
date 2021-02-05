11ba8f2a01df21f79db784dae2e9377564797798
Jason Wong <jpwong@ucsd.edu>
2021-02-05 05:07:58 -0800

    second test

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,34 +10,36 @@ volSli.addEventListener("change", numToSli);
 
 function numToSli() {
     volNum.value = volSli.value;
+    horn.volume = volNum.valueAsNumber/100;
 }
 function sliToNum() {
     volSli.value = volNum.value;
+    horn.volume = volSli.valueAsNumber/100;
 }
 
 var horn = document.getElementById("horn-sound");
-horn.volume = volNum/100;
 
 var icon = document.getElementById("volume-image");
+var button = document.getElementById("honk-btn");
 horn.addEventListener("volume-change", changeIcon);
 
 function changeIcon() {
-    if (volNum.value <= 100 && volNum.value >= 67) {
+    if (volNum.valueAsNumber <= 100 && volNum.valueAsNumber >= 67) {
         icon.src = "./assets/media/icons/volume-level-3.svg";
         icon.alt = "Max Volume";
         button.disabled = false;
     }
-    else if (volNum.value < 67 && volNum.value >= 34) {
+    else if (volNum.valueAsNumber < 67 && volNum.valueAsNumber >= 34) {
         icon.src = "./assets/media/icons/volume-level-2.svg";
         icon.alt = "Medium Volume";
         button.disabled = false;
     }
-    else if (volNum.value < 34 && volNum.value >= 1) {
+    else if (volNum.valueAsNumber < 34 && volNum.valueAsNumber >= 1) {
         icon.src = "./assets/media/icons/volume-level-1.svg";
         icon.alt = "Low Volume";
         button.disabled = false;
     }
-    else if (volNum.value == 0) {
+    else if (volNum.valueAsNumber == 0) {
         icon.src = "./assets/media/icons/volume-level-0.svg";
         icon.alt = "No Volume";
         button.disabled = true;
@@ -45,32 +47,31 @@ function changeIcon() {
 }
 
 var sndImg = document.getElementById("sound-image");
-var radio = document.getElementById("radio-air-horn");
+var radio = document.getElementsByName("radio-sound");
 radio.addEventListener("change", changeHorn);
 
 function changeHorn() {
-    if (radio.label == "Air Horn") {
+    if (radio[0].checked == true) {
         sndImg.src = "./assets/media/images/air-horn.svg";
         sndImg.alt = "Air Horn";
         horn.src = "./assets/media/audio/air-horn.mp3";
     }
-    else if (radio.label == "Car Horn") {
+    else if (radio[1].checked == true) {
         sndImg.src = "./assets/media/images/car.svg";
         sndImg.alt = "Car Horn";
         horn.src = "./assets/media/audio/car-horn.mp3";
     }
-    else if (radio.label == "Party Horn") {
+    else if (radio[2].checked == true) {
         sndImg.src = "./assets/media/images/party-horn.svg";        
         sndImg.alt = "Party Horn";
         horn.src = "./assets/media/audio/party-horn.mp3";
     }
 }
 
-var button = document.getElementById("honk-btn");
 button.addEventListener("click", honk);
 
 function honk() {
-    horn.autoplay = true;
+    horn.play();
 }
 
 
